bd66909b07411d7a45db83b64790742797d8b640
GokedaLokesh <lokeshgokeda551@gmail.com>
2026-09-08 21:44:54 +0530

    Add files via upload

---
+++ DAY 1.py
@@ -0,0 +1,72 @@
+
+print("Hello World")
+print("it is my first youtube video")
+print(2+3)
+print(34 * 45)
+# variables
+name = "Lokesh"
+age_1 = 19
+print(name)
+print(age_1)
+name = "Lokesh"
+Name = "loki"
+print(name)
+print(Name)
+#python data types
+#String
+name = "lokesh"
+print(name)
+print(type(name))
+#int
+age = 20
+print(age)
+print(type(age))
+#float
+height = 5.11 
+print(height)
+print(type(height))
+#boolean
+is_student = True 
+print(type(is_student))
+#Basic operators 
+#+,-,*,/,%
+#taking user input
+a = int(input("enter a value"))
+b = int(input("enter b value"))
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+#power 
+print(a ** b)
+
+#type conversion
+age = float(19)
+height = int(5.11)
+#type conversions
+print(age)
+print(type(age))
+print(height)
+print(type(height))
+#string concatenation
+first_name = "Lokesh"
+last_name = "GOKEDA"
+print(first_name +  last_name)
+#we can also use f strings
+name = "Lokesh"
+age = 19
+print(f"My name is {name} and I am {age} years old")
+
+#project 1
+#Simple calculator 
+#try it yourself
+#project 2
+#simple interset 
+#we know simple interset formula = ptr/100 by using this we can calculate simple interest
+print("simple interset calculator")
+p = int(input("enter principal amount"))
+t = int(input("enter time in years"))
+r = float(input("enter rate of interest"))#() has most higher than *,/
+simple_interest = (p * t * r) /100#presdence /,* has higher 
+print(simple_interest)# +-
